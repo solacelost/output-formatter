30b290e546715b1e802d7c418dd7d22e3987b2fe
James Harmison <jharmison@gmail.com>
2019-09-25 10:37:13 -0400

    Added some tweaks to the example bootstrap for more flexibility

--- example-bootstrap.sh
+++ example-bootstrap.sh
@@ -4,6 +4,14 @@
 #   formatter itself in your release/repo.
 # Ensure you .gitignore it if appropriate!
 
+# Change this as needed to point to the release you're wanting.
+formatter_url=https://raw.githubusercontent.com/solacelost/output-formatter/v1.0/formatter
+
+# Always start with a sane place for formatter, don't leave copies everywher
+original_dir=$(pwd)
+cd $(dirname $(realpath $0))
+# Alternative download locations are things like ~/.local/share (very reliable)
+#   or /tmp (very ephemeral, may trip up some policies)
 
 # Figure out what we can use to download things
 function which_dl() {
@@ -20,17 +28,20 @@ function which_dl() {
 
 # Downloads a release of the output-formatter to make things pretty
 if [ ! -r formatter ]; then
-    formatter_url=https://raw.githubusercontent.com/solacelost/output-formatter/v1.0/formatter
     # Try to download the thing
     if ! $(which_dl formatter) "$formatter_url" 2>/dev/null; then
-        echo 'Unable to download formatter, please ensure you have network' \
-             'connectivity or stage formatter from the following link' \
-             'adjacent to mkvenv.sh:' >&2
+        echo "Unable to download formatter, please ensure you have network" \
+             "connectivity and write permissions to $(pwd), or stage" \
+             "formatter from the following link adjacent to mkvenv.sh:" >&2
         echo "$formatter_url" >&2
         exit 7
     fi
 fi
 . formatter
+
+# From here, we should go back where you came from
+cd "$original_dir"
+
 # From here on you have all the functions of the formatter at your disposal
 
 center_border_text "Everything appears to have worked!"
